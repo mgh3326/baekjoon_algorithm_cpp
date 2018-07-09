ca27420f268fb231f22884324a06a9effc82cbd7
mgh3326 <mgh3326@naver.com>
2018-07-09 13:15:18 +0900

    평균은 넘겠지 해야됨

---
+++ if문 사용해보기/평균은넘겠지/평균은넘겠지/Source.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+	int c = 0;
+	scanf("%d", &c);
+	int temp = 0;
+	int num = 0;
+	for (int i = 0; i < c; i++)
+	{
+		scanf("%d", &temp);
+		for (int i = 0; i < temp; i++)
+		{
+			scanf("%d", &num);
+		}
+	}
+}
