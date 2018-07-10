072f4b14725961e13dfa5461b081056e2fb9bf80
mgh3326 <mgh3326@naver.com>
2018-07-10 17:32:23 +0900

    0710 update

---
+++ 1차원_배열_사용하기/단어의개수/단어의개수/Source.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+	char c = '\0';
+	int answer = 1;
+	char name[1000004];
+	std::cin.getline(name, 1000004);
+	int index = 0;
+	if (name[0] == '\0' || name[1] == '\0') {
+		answer = 0;
+	}
+	if (name[0] == ' ') {
+		answer = 0;
+	}
+	while (1) {
+		if (name[index] == '\0')
+			break;
+		if (name[index] == ' ') {
+			if (name[index + 1] == '\0')
+				break;
+			answer++;
+		}
+		index++;
+	}
+	printf("%d\n", answer);
+}
---
+++ 함수사용하기/별찍기/별찍기/Source.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+	/*for (int i = 0; i < 10; i++)
+	{
+		printf("%d\n",3*int(pow(2,i)));
+	}
+	*/
+	int n = 0;
+	//scanf("%d", &n);
+	n = 3;
+	for (int i = 0; i < n; i++)
+	{
+		printf(" ");
+	}
+	printf("*\n");
+
+}
