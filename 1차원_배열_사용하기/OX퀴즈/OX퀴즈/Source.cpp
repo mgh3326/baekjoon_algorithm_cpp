3921bf4b529cf6c6605d75c9c8203b8b0d908ebb
mgh3326 <mgh3326@naver.com>
2018-09-13 02:22:14 +0900

    cpp version update

--- 1차원_배열_사용하기/OX퀴즈/OX퀴즈/Source.cpp
+++ 1차원_배열_사용하기/OX퀴즈/OX퀴즈/Source.cpp
@@ -1,6 +1,5 @@
-
-#include <stdio.h> 
-
+#include <iostream>
+using namespace std;
 int main(void) {
 	int num = 0;
 	scanf("%d", &num);
@@ -8,35 +7,27 @@ int main(void) {
 	int index;
 	int weight;
 	int score;
-	for (int i = 0; i < num + 1; i++)//처음에 엔터값 들어와서 그런가
+	for (int i = 0; i < num; i++)//처음에 엔터값 들어와서 그런가
 	{
+		scanf("%s", str);
 
-		gets(str);
-		if (str[0] == 0)
-			continue;
 		index = 0;
 		weight = 0;
 		score = 0;
 		while (1) {
 			if (str[index] == 0)
 				break;
-			if (str[index] == 79)
+			else if (str[index] == 79)
 			{
-				/*	printf("O나왔네\n");*/
 				weight++;
 				score += weight;
 			}
 			else if (str[index] == 88)
 			{
-				/*printf("X나왔네\n");*/
 				weight = 0;
 			}
 			index++;
 		}
 		printf("%d\n", score);
-
-		/*printf("1233");*/
 	}
-	/*printf("%d\n", answer);*/
-	/*return 0;*/
 }
