0e7ba7676659657326212006c8e003ac0ab3308a
Moonkwanghyun <mgh3326@naver.com>
2018-11-10 21:37:47 +0900

    1110 update

--- 문자열_사용하기/크로아티아_알파벳/크로아티아_알파벳/Source.cpp
+++ 문자열_사용하기/크로아티아_알파벳/크로아티아_알파벳/Source.cpp
@@ -9,25 +9,41 @@ int main(void) {
 			break;
 		if (str[index] == '=') {
 			if (str[index - 1] == 'c') {//c=
-
+				count -= 1;
 			}
 			else if (str[index - 1] == 'z') {
 				if (str[index - 2] == 'd') {//dz=
-
+					count -= 2;
 				}
 				else {//z=
-
+					count -= 1;
 				}
+			}
+			else if (str[index - 1] == 's') {//s=
+				count -= 1;
 			}
 		}
 		else if (str[index] == '-') {
-
+			if (str[index - 1] == 'c') {//c-
+				count -= 1;
+			}
+			else if (str[index - 1] == 'd') {//d-
+				count -= 1;
+			}
 		}
 		else if (str[index] == 'j') {
-
+			if (str[index - 1] == 'l') {//lj
+				count -= 1;
+			}
+			else if (str[index - 1] == 'n') {//nj
+				count -= 1;
+			}
 		}
-		else {
+		/*else {
 			count++;
-		}
+		}*/
+		index++;
+		count++;
 	}
+	printf("%d\n", count);
 }
