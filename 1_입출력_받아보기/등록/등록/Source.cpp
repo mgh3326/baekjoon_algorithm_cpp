00f508e7d5c342a3313f62e33210c3ab34fbd19d
Kwanghyun Moon <mgh3326@naver.com>
2019-01-28 15:05:20 +0900

    Folder name remove

--- 1_입출력_받아보기/등록/등록/Source.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(void) {
-	cout << "7\nmgh3326" << endl;
-}
--- 7_문자열_사용하기/아스키_코드/아스키_코드/Source.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(void) {
-	char input = 0;
-	scanf("%c", &input);
-	printf("%d\n", input);
-	return 0;
-}
